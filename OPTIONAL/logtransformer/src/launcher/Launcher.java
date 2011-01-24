b22c6101e60431e8a1492ba2a34b839279ea78a8
Leo Roos <leor.email.googlemail.com>
2011-01-24 02:27:18 +0000

    setting up independent tigerseye-plugin location

---
+++ OPTIONAL/logtransformer/src/launcher/Launcher.java
@@ -0,0 +1,11 @@
+package launcher;
+
+import de.lroos.Main;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// Main.main(new String[] { "test" });
+		Main.main(new String[] { "test", "testout" });
+	}
+}
