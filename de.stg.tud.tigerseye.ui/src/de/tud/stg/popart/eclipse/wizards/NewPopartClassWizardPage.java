67598173c5665a4389abaf6431f81dd3b76bc0d5
Leo Roos <leor.email.googlemail.com>
2011-01-31 23:23:39 +0000

    - added win dependencies to launch. - no new class definition if no active dsl

--- de.stg.tud.tigerseye.ui/src/de/tud/stg/popart/eclipse/wizards/NewPopartClassWizardPage.java
+++ de.stg.tud.tigerseye.ui/src/de/tud/stg/popart/eclipse/wizards/NewPopartClassWizardPage.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.CheckForNull;
 
@@ -56,31 +57,28 @@ public class NewPopartClassWizardPage extends NewClassWizardPage {
 
     private static final Logger logger = LoggerFactory
 	    .getLogger(NewPopartClassWizardPage.class);
-	private IJavaProject project;
-	private final ArtifactCodeGenerator codeGenerator;
-	private final ImportManager importManager;
-	private Combo combo;
+    private IJavaProject project;
+    private final ArtifactCodeGenerator codeGenerator;
+    private final ImportManager importManager;
+    private Combo combo;
     private final Map<String, DSLDefinition> dsls = new HashMap<String, DSLDefinition>();
 
-    public NewPopartClassWizardPage() {
+    public NewPopartClassWizardPage(Set<DSLDefinition> activeDSLs) {
 	super();
 	setTitle("Tigerseye Class");
 	setDescription("Create a new Tigerseye class");
 	codeGenerator = new ArtifactCodeGenerator(project);
 	importManager = new ImportManager(project);
-	List<DSLDefinition> dslDefinitions = TigerseyeCore
-		.getLanguageProvider().getDSLDefinitions();
-	dsls.clear();
-	for (DSLDefinition dsl : dslDefinitions) {
+	for (DSLDefinition dsl : activeDSLs) {
+	    String extension;
 	    try {
-		if (dsl.isActive()) {
-		    String extension = dsl.getValue(DSLKey.EXTENSION);
-		    dsls.put(extension, dsl);
-		}
+		extension = dsl.getValue(DSLKey.EXTENSION);
+		dsls.put(extension, dsl);
 	    } catch (NoLegalPropertyFound e) {
-		// No extension defined. Can be ignored here.
+		// Can be safely ignored
 	    }
 	}
+
     }
 
     @Override
@@ -93,190 +91,194 @@ public class NewPopartClassWizardPage extends NewClassWizardPage {
 	return dsls;
     }
 
-	@Override
-	public void init(IStructuredSelection selection) {
-		super.init(selection);
-		IJavaElement jelem = getInitialJavaElement(selection);
+    @Override
+    public void init(IStructuredSelection selection) {
+	super.init(selection);
+	IJavaElement jelem = getInitialJavaElement(selection);
 	if (jelem == null)
 	    MessageDialog.openInformation(getShell(), "Need selection",
 		    "Select the project first where to add the new  class");
-		this.project = jelem.getJavaProject();
-	}
+	this.project = jelem.getJavaProject();
+    }
 
-	public IFile createGroovyType(IProgressMonitor monitor)
-			throws CoreException {
+    public IFile createGroovyType(IProgressMonitor monitor)
+	    throws CoreException {
 
 	monitor.beginTask("Creating Tigerseye Class...", 2);
-		IPackageFragment packageFragment = getPackageFragment();
-		List<String> superInterfaces = getSuperInterfaces();
-		getModifiers();
+	IPackageFragment packageFragment = getPackageFragment();
+	List<String> superInterfaces = getSuperInterfaces();
+	getModifiers();
 
-		monitor.worked(1);
+	monitor.worked(1);
 
-		codeGenerator.clear();
-		importManager.clear();
+	codeGenerator.clear();
+	importManager.clear();
 
-		importManager.setBasePackage(getPackageText());
+	importManager.setBasePackage(getPackageText());
 
-		// Print popart-language information
+	// Print popart-language information
 	printDeclaredMethods(getDsls().get(combo.getText()));
 
-		codeGenerator.addLineBreak().addCode("##imports##").addLineBreak();
+	codeGenerator.addLineBreak().addCode("##imports##").addLineBreak();
 
-		codeGenerator.addCode(
-				getPopartExtensionName() + "(name:'" + getTypeName() + "'){")
-				.addLineBreak();
+	codeGenerator.addCode(
+		getPopartExtensionName() + "(name:'" + getTypeName() + "'){")
+		.addLineBreak();
 
-		if (isCreateConstructors()) {
-			if (getGroovySuperClass() != null) {
-				IType type = project.findType(getGroovySuperClass());
-				if (type != null) {
-					codeGenerator
-							.addCode(IndentationDirection.INDENT_RIGHT, "");
-					for (int i = 0; i < type.getMethods().length; i++) {
-						IMethod method = type.getMethods()[i];
-						int mflags = method.getFlags();
+	if (isCreateConstructors()) {
+	    if (getGroovySuperClass() != null) {
+		IType type = project.findType(getGroovySuperClass());
+		if (type != null) {
+		    codeGenerator
+			    .addCode(IndentationDirection.INDENT_RIGHT, "");
+		    for (int i = 0; i < type.getMethods().length; i++) {
+			IMethod method = type.getMethods()[i];
+			int mflags = method.getFlags();
 
-						if (method.isConstructor()
-								&& (Flags.isPublic(mflags) || Flags
-										.isProtected(mflags))) {
-							CodeGeneration.handleInheritedMethod(project,
-									method, codeGenerator, importManager,
-									getTypeName(), getPackageLabel());
-						}
-					}
-				} else {
-		    logger.warn("Type <" + getGroovySuperClass()
-							+ "> not found");
-				}
-			} else {
-				codeGenerator.addCode(
-						IndentationDirection.INDENT_RIGHT,
-						CodeGeneration.getMethodComment(project, getTypeName(),
-								getPackageText(), getTypeName(), new String[0],
-								"V"))
-						.addCode("public " + getTypeName() + "(){").addCode(
-								IndentationDirection.INDENT_RIGHT,
-								CodeGeneration
-										.getMethodBodyContent(project,
-												getTypeName(), getTypeName(),
-												false, "")).addCode(
-								IndentationDirection.INDENT_LEFT, "}");
+			if (method.isConstructor()
+				&& (Flags.isPublic(mflags) || Flags
+					.isProtected(mflags))) {
+			    CodeGeneration.handleInheritedMethod(project,
+				    method, codeGenerator, importManager,
+				    getTypeName(), getPackageLabel());
 			}
-			codeGenerator.addCode(IndentationDirection.INDENT_LEFT, "");
-		}
-
-		if (isCreateMain()) {
-			codeGenerator.addLineBreak().addCode(
-					IndentationDirection.INDENT_RIGHT,
-					CodeGeneration.getMethodComment(project, getTypeName(),
-							getPackageText(), "main", new String[] { "args" },
-							"V")).addCode("public static void main(def args){")
-					.addCode(
-							IndentationDirection.INDENT_RIGHT,
-							CodeGeneration.getMethodBodyContent(project,
-									getTypeName(), "main", false, "")).addCode(
-							IndentationDirection.INDENT_LEFT, "}");
+		    }
 		} else {
-			codeGenerator.addCode(IndentationDirection.INDENT_RIGHT, "");
+		    logger.warn("Type <" + getGroovySuperClass()
+			    + "> not found");
 		}
-		codeGenerator.addLineBreak();
-
-		if (!superInterfaces.isEmpty() && isCreateInherited()) {
-			for (int i = 0; i < superInterfaces.size(); i++) {
-				IType type = project.findType(superInterfaces.get(i));
-				if (type != null) {
-					getInheritedMethods(type);
-				} else {
-		    logger.warn("Type <" + superInterfaces.get(i)
-							+ "> not found");
-				}
-			}
-		}
-
-		if (getGroovySuperClass() != null && isCreateInherited()) {
-			IType type = project.findType(getGroovySuperClass());
-
-			if (type != null) {
-				for (int i = 0; i < type.getMethods().length; i++) {
-					IMethod method = type.getMethods()[i];
-					int mflags1 = method.getFlags();
-
-					if (Flags.isAbstract(mflags1)
-							&& (Flags.isPublic(mflags1) || Flags
-									.isProtected(mflags1))) {
-						CodeGeneration.handleInheritedMethod(project, method,
-								codeGenerator, importManager, getTypeName(),
-								getPackageLabel());
-					}
-				}
-			} else {
-		logger.warn("Type <" + getGroovySuperClass()
-						+ "> not found");
-			}
-		}
-
-		codeGenerator.addCode(IndentationDirection.INDENT_LEFT, "}");
-
-		String sourceCode = codeGenerator.toString();
-		sourceCode = sourceCode.replaceAll("##imports##", importManager
-				.toString());
-		sourceCode = sourceCode.replaceAll("##generics##", importManager
-				.getGenericTypesAsString());
-
-		monitor.worked(1);
-
-		monitor.done();
-
-		return WizardUtil.createGroovyType(getPackageFragmentRoot(),
-				packageFragment, getTypeName() + "." + getPopartExtensionName()
-						+ ".dsl", sourceCode);
+	    } else {
+		codeGenerator
+			.addCode(
+				IndentationDirection.INDENT_RIGHT,
+				CodeGeneration.getMethodComment(project,
+					getTypeName(), getPackageText(),
+					getTypeName(), new String[0], "V"))
+			.addCode("public " + getTypeName() + "(){")
+			.addCode(
+				IndentationDirection.INDENT_RIGHT,
+				CodeGeneration
+					.getMethodBodyContent(project,
+						getTypeName(), getTypeName(),
+						false, ""))
+			.addCode(IndentationDirection.INDENT_LEFT, "}");
+	    }
+	    codeGenerator.addCode(IndentationDirection.INDENT_LEFT, "");
 	}
+
+	if (isCreateMain()) {
+	    codeGenerator
+		    .addLineBreak()
+		    .addCode(
+			    IndentationDirection.INDENT_RIGHT,
+			    CodeGeneration.getMethodComment(project,
+				    getTypeName(), getPackageText(), "main",
+				    new String[] { "args" }, "V"))
+		    .addCode("public static void main(def args){")
+		    .addCode(
+			    IndentationDirection.INDENT_RIGHT,
+			    CodeGeneration.getMethodBodyContent(project,
+				    getTypeName(), "main", false, ""))
+		    .addCode(IndentationDirection.INDENT_LEFT, "}");
+	} else {
+	    codeGenerator.addCode(IndentationDirection.INDENT_RIGHT, "");
+	}
+	codeGenerator.addLineBreak();
+
+	if (!superInterfaces.isEmpty() && isCreateInherited()) {
+	    for (int i = 0; i < superInterfaces.size(); i++) {
+		IType type = project.findType(superInterfaces.get(i));
+		if (type != null) {
+		    getInheritedMethods(type);
+		} else {
+		    logger.warn("Type <" + superInterfaces.get(i)
+			    + "> not found");
+		}
+	    }
+	}
+
+	if (getGroovySuperClass() != null && isCreateInherited()) {
+	    IType type = project.findType(getGroovySuperClass());
+
+	    if (type != null) {
+		for (int i = 0; i < type.getMethods().length; i++) {
+		    IMethod method = type.getMethods()[i];
+		    int mflags1 = method.getFlags();
+
+		    if (Flags.isAbstract(mflags1)
+			    && (Flags.isPublic(mflags1) || Flags
+				    .isProtected(mflags1))) {
+			CodeGeneration.handleInheritedMethod(project, method,
+				codeGenerator, importManager, getTypeName(),
+				getPackageLabel());
+		    }
+		}
+	    } else {
+		logger.warn("Type <" + getGroovySuperClass() + "> not found");
+	    }
+	}
+
+	codeGenerator.addCode(IndentationDirection.INDENT_LEFT, "}");
+
+	String sourceCode = codeGenerator.toString();
+	sourceCode = sourceCode.replaceAll("##imports##",
+		importManager.toString());
+	sourceCode = sourceCode.replaceAll("##generics##",
+		importManager.getGenericTypesAsString());
+
+	monitor.worked(1);
+
+	monitor.done();
+
+	return WizardUtil.createGroovyType(getPackageFragmentRoot(),
+		packageFragment, getTypeName() + "." + getPopartExtensionName()
+			+ ".dsl", sourceCode);
+    }
 
     private String getGroovySuperClass() {
 	logger.error("Accessing no operation supporting method getGroovySuperClass");
 	return null;
     }
 
-	private void getInheritedMethods(IType type) throws CoreException {
-		IMethod[] methods = type.getMethods();
+    private void getInheritedMethods(IType type) throws CoreException {
+	IMethod[] methods = type.getMethods();
 
-		for (int i = 0; i < methods.length; i++) {
-			CodeGeneration.handleInheritedMethod(project, methods[i],
-					codeGenerator, importManager, getTypeName(),
-					getPackageLabel());
-		}
+	for (int i = 0; i < methods.length; i++) {
+	    CodeGeneration.handleInheritedMethod(project, methods[i],
+		    codeGenerator, importManager, getTypeName(),
+		    getPackageLabel());
 	}
+    }
 
-	private String getPopartExtensionName() {
-		if (combo == null)
-			return "";
-		return combo.getText();
-	}
+    private String getPopartExtensionName() {
+	if (combo == null)
+	    return "";
+	return combo.getText();
+    }
 
-	@Override
-	protected void createTypeNameControls(Composite composite, int columns) {
-		super.createTypeNameControls(composite, 3);
+    @Override
+    protected void createTypeNameControls(Composite composite, int columns) {
+	super.createTypeNameControls(composite, 3);
 
-		Composite innerComposite = new Composite(composite, SWT.NONE);
-		innerComposite.setLayout(new GridLayout(2, false));
+	Composite innerComposite = new Composite(composite, SWT.NONE);
+	innerComposite.setLayout(new GridLayout(2, false));
 
-		combo = new Combo(innerComposite, SWT.READ_ONLY);
-		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+	combo = new Combo(innerComposite, SWT.READ_ONLY);
+	combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 
-		setLanguagesExtensionFromStore();
+	setLanguagesExtensionFromStore();
 
-		Label b = new Label(innerComposite, SWT.NONE);
-		b.setLayoutData(new GridData());
-		b.setText(".dsl");
+	Label b = new Label(innerComposite, SWT.NONE);
+	b.setLayoutData(new GridData());
+	b.setText(".dsl");
 
-	}
+    }
 
-	/**
-	 * This method adds all existing extensions from store to combo-box
-	 * selection model, which is used for selection DSL extension by creating of
-	 * Popart classes.
-	 */
+    /**
+     * This method adds all existing extensions from store to combo-box
+     * selection model, which is used for selection DSL extension by creating of
+     * Popart classes.
+     */
     private void setLanguagesExtensionFromStore() {
 	Collection<DSLDefinition> values = getDsls().values();
 	for (DSLDefinition extension : values) {
@@ -294,65 +296,64 @@ public class NewPopartClassWizardPage extends NewClassWizardPage {
 	combo.select(0);
     }
 
-	/**
-	 * This method prints keywords of language for the given class-path.
-	 * 
-	 * @param externalClassPath
-	 *            the path from a class with methods that should be found
-	 */
+    /**
+     * This method prints keywords of language for the given class-path.
+     * 
+     * @param externalClassPath
+     *            the path from a class with methods that should be found
+     */
     private void printDeclaredMethods(DSLDefinition dsl) {
 
-		// Print Language classpath
-		codeGenerator.addLineBreak().addCode("/**");
+	// Print Language classpath
+	codeGenerator.addLineBreak().addCode("/**");
 	codeGenerator.addCode(" * Tigerseye language: " + dsl.getClassPath());
-		codeGenerator.addCode(" *");
-		codeGenerator.addCode(" * Declared keywords:");
+	codeGenerator.addCode(" *");
+	codeGenerator.addCode(" * Declared keywords:");
 
-		// Read all public declared fields from external class
+	// Read all public declared fields from external class
 	Field[] declaredFields = new KeyWordExtractor()
 		.getDeclaredLiteralKeywords(dsl.loadClass());
-		if (declaredFields != null) {
-			for (Field publicDeclaredField : declaredFields) {
+	if (declaredFields != null) {
+	    for (Field publicDeclaredField : declaredFields) {
 
-				String printedLine = publicDeclaredField.getName();
+		String printedLine = publicDeclaredField.getName();
 
+		printedLine = publicDeclaredField.getType().getSimpleName()
+			+ " " + printedLine;
 
-				printedLine = publicDeclaredField.getType()
-						.getSimpleName()
-						+ " " + printedLine;
-
-				codeGenerator.addCode(" *  " + printedLine);
-			}
-		}
-		
-		// Read all public declared methods from external class
-	Method[] declaredMethods = new KeyWordExtractor()
-.getMethodKeywords(dsl
-		.loadClass());
-		if (declaredMethods != null) {
-			for (Method publicDeclaredMethod : declaredMethods) {
-
-				String printedLine = publicDeclaredMethod.getName();
-
-				String parameters = "";
-				for (int i = 0; i < publicDeclaredMethod.getParameterTypes().length; i++) {
-					if (i > 0)
-						parameters = parameters + ", ";
-					parameters = parameters
-							+ publicDeclaredMethod.getParameterTypes()[i]
-									.getSimpleName().toString();
-				}
-
-				printedLine = publicDeclaredMethod.getReturnType()
-						.getSimpleName()
-						+ " " + printedLine + ("(" + parameters + ")");
-
-				codeGenerator.addCode(" *  " + printedLine);
-
-			}
-		}
-		codeGenerator.addCode(" */").addLineBreak();
+		codeGenerator.addCode(" *  " + printedLine);
+	    }
 	}
+
+	// Read all public declared methods from external class
+	Method[] declaredMethods = new KeyWordExtractor().getMethodKeywords(dsl
+		.loadClass());
+	if (declaredMethods != null) {
+	    for (Method publicDeclaredMethod : declaredMethods) {
+
+		String printedLine = publicDeclaredMethod.getName();
+
+		String parameters = "";
+		for (int i = 0; i < publicDeclaredMethod.getParameterTypes().length; i++) {
+		    if (i > 0)
+			parameters = parameters + ", ";
+		    parameters = parameters
+			    + publicDeclaredMethod.getParameterTypes()[i]
+				    .getSimpleName().toString();
+		}
+
+		printedLine = publicDeclaredMethod.getReturnType()
+			.getSimpleName()
+			+ " "
+			+ printedLine
+			+ ("(" + parameters + ")");
+
+		codeGenerator.addCode(" *  " + printedLine);
+
+	    }
+	}
+	codeGenerator.addCode(" */").addLineBreak();
+    }
 
     public @CheckForNull
     IJavaProject getProject() {
--- de.stg.tud.tigerseye.ui/src/de/tud/stg/popart/eclipse/wizards/NewPopartClassWizzard.java
+++ de.stg.tud.tigerseye.ui/src/de/tud/stg/popart/eclipse/wizards/NewPopartClassWizzard.java
@@ -1,16 +1,23 @@
 package de.tud.stg.popart.eclipse.wizards;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.internal.ui.wizards.NewElementWizard;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.PlatformUI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.tud.stg.tigerseye.eclipse.core.DSLDefinition;
+import de.tud.stg.tigerseye.eclipse.core.TigerseyeCore;
 import de.tud.stg.tigerseye.eclipse.core.TigerseyeRuntime;
 
 /**
@@ -23,29 +30,44 @@ import de.tud.stg.tigerseye.eclipse.core.TigerseyeRuntime;
  * @author Leonid Melnyk
  */
 public class NewPopartClassWizzard extends NewElementWizard {
-	private NewPopartClassWizardPage fPage;
+    private NewPopartClassWizardPage fPage;
 
     private static final Logger logger = LoggerFactory
 	    .getLogger(NewPopartClassWizzard.class);
 
-	public NewPopartClassWizzard() {
-		super();
+    public NewPopartClassWizzard() {
+	super();
 	setWindowTitle("Create a new Tigerseye class");
-	}
+    }
 
-	@Override
-	public void addPages() {
-		fPage = new NewPopartClassWizardPage();
-		addPage(fPage);
-		fPage.init(getSelection());
+    @Override
+    public void addPages() {
+
+	List<DSLDefinition> dslDefinitions = TigerseyeCore
+		.getLanguageProvider().getDSLDefinitions();
+	Set<DSLDefinition> activeDSLs = new HashSet<DSLDefinition>();
+	for (DSLDefinition dsl : dslDefinitions) {
+	    if (dsl.isActive()) {
+		activeDSLs.add(dsl);
+	    }
 	}
+	if (activeDSLs.size() < 1) {
+	    MessageDialog
+		    .openInformation(getShell(), "No active DSL",
+			    "At least one dsl must have been activated in order to create a new one.");
+	    addPage(null);
+	    return;
+	}
+	fPage = new NewPopartClassWizardPage(activeDSLs);
+	addPage(fPage);
+	fPage.init(getSelection());
+    }
 
     private IFile file;
-	@Override
+
+    @Override
     protected void finishPage(final IProgressMonitor monitor)
 	    throws CoreException {
-
-	
 
 	PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
 
@@ -57,8 +79,7 @@ public class NewPopartClassWizzard extends NewElementWizard {
 
 		    openResource(file);
 
-		    TigerseyeRuntime
-.addTigerseyeDSLUsingConfiguration(fPage
+		    TigerseyeRuntime.addTigerseyeDSLUsingConfiguration(fPage
 			    .getProject().getProject());
 
 		} catch (CoreException e) {
@@ -67,8 +88,7 @@ public class NewPopartClassWizzard extends NewElementWizard {
 	    }
 	});
 
-
-	}
+    }
 
     @Override
     public IJavaElement getCreatedElement() {
