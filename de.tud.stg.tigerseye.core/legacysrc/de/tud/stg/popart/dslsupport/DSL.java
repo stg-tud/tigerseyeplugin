9d36ca50fbf3b4a888b8718f6c3adde106f4ad8b
Leo Roos <leor.email.googlemail.com>
2011-02-06 19:42:17 +0000

    renamed src folder which contains the classes used by DSLs

--- de.tud.stg.tigerseye.core/legacysrc/de/tud/stg/popart/dslsupport/DSL.java
+++
@@ -1,9 +0,0 @@
-package de.tud.stg.popart.dslsupport;
-
-/**
- * Marker interface. Identifies a DSL defining class.
- *
- */
-public interface DSL {
-
-}
--- de.tud.stg.tigerseye.core/legacysrc/de/tud/stg/popart/eclipse/core/debug/handler/IObserver.java
+++
@@ -1,7 +0,0 @@
-package de.tud.stg.popart.eclipse.core.debug.handler;
-
-public interface IObserver {
-
-	public void update();
-	
-}
