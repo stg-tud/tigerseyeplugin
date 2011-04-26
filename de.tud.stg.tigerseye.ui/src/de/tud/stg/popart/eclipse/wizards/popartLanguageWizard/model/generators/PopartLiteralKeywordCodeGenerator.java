4fa749fc5aa1bc91e80b45b6271903005482d2de
Leo Roos <leor.email.googlemail.com>
2011-04-26 09:48:48 +0000

    adjusting folder name analogous to the id

---
+++ de.tud.stg.tigerseye.ui/src/de/tud/stg/popart/eclipse/wizards/popartLanguageWizard/model/generators/PopartLiteralKeywordCodeGenerator.java
@@ -0,0 +1,92 @@
+package de.tud.stg.popart.eclipse.wizards.popartLanguageWizard.model.generators;
+
+import legacy.org.codehaus.groovy.eclipse.ui.ArtifactCodeGenerator;
+import legacy.org.codehaus.groovy.eclipse.ui.ArtifactCodeGenerator.IndentationDirection;
+import legacy.org.codehaus.groovy.eclipse.wizards.WizardUtil;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.tud.stg.popart.eclipse.core.debug.model.keywords.PopartLiteralKeyword;
+import de.tud.stg.popart.eclipse.wizards.popartLanguageWizard.model.IPopartKeywordCodeGenerator;
+
+/**
+ * A code generator for PopartLiteralKeywords.
+ * A PopartLiteralKeywordCodeGenerator is capable of generating
+ * source code for a Popart language defintion from PopartLiteralKeywords.
+ *
+ * @author David Marx
+ * @author Thorsten Peter
+ */
+public class PopartLiteralKeywordCodeGenerator implements IPopartKeywordCodeGenerator {
+
+    private static final Logger logger = LoggerFactory
+	    .getLogger(PopartLiteralKeywordCodeGenerator.class);
+
+	private final PopartLiteralKeyword keyword;
+	
+	/**
+	 * Constructs a new PopartLiteralKeywordCodeGenerator for the
+	 * specified PopartLiteralKeyword.
+	 * 
+	 * @param literalKeyword The PopartLiteralKeyword
+	 */
+	public PopartLiteralKeywordCodeGenerator(PopartLiteralKeyword literalKeyword) {
+		this.keyword = literalKeyword;
+	}
+
+	/**
+	 * @see de.tud.stg.popart.eclipse.wizards.popartLanguageWizard.model.IPopartKeywordCodeGenerator
+	 */
+	@Override
+	public void create(IJavaProject project, IPackageFragment packagefragment,
+			IPackageFragmentRoot root, ArtifactCodeGenerator codeGenerator) {
+
+		String name = keyword.getName();
+		String type = keyword.getType();
+		
+		String supertype = "";
+		if (type==null || type.equals("")) {
+			 supertype = name;
+		}
+		else {
+			supertype = type;
+		}
+		codeGenerator.addCode(IndentationDirection.INDENT_RIGHT, "");	
+		codeGenerator.addCode("public "+supertype+" "+name+" = new "+name+"();");
+		codeGenerator.addLineBreak();
+		codeGenerator.addCode(IndentationDirection.INDENT_LEFT, "");
+		
+		ArtifactCodeGenerator myGenerator = new ArtifactCodeGenerator(project);
+
+		// create supertype as abstract class if not already done.
+		if (type!=null && !type.equals("")) {
+			WizardUtil.createType(project, packagefragment, root, type, true);
+		}		
+		
+		myGenerator.clear();
+		
+		String temp = "public class " + name;
+		if (type!=null && !type.equals("")) {
+			temp += " extends " + type;
+		}
+		temp += " { ";
+		myGenerator.addCode(temp);		
+		
+		myGenerator.addLineBreak();
+		myGenerator.addLineBreak();
+		myGenerator.addCode("}");
+
+	try {
+			WizardUtil.createGroovyType(root, packagefragment,
+					name + ".groovy", myGenerator.toString());
+	} catch (CoreException e) {
+	    logger.error("Wizard Util access.", e);
+	}
+	}
+	
+}
---
+++ de.tud.stg.tigerseye.ui/src/de/tud/stg/popart/eclipse/wizards/popartLanguageWizard/model/generators/PopartOperationKeywordCodeGenerator.java
@@ -0,0 +1,78 @@
+package de.tud.stg.popart.eclipse.wizards.popartLanguageWizard.model.generators;
+
+import legacy.org.codehaus.groovy.eclipse.ui.ArtifactCodeGenerator;
+import legacy.org.codehaus.groovy.eclipse.ui.ArtifactCodeGenerator.IndentationDirection;
+import legacy.org.codehaus.groovy.eclipse.wizards.WizardUtil;
+
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+
+import de.tud.stg.popart.eclipse.core.debug.model.keywords.PopartOperationKeyword;
+import de.tud.stg.popart.eclipse.wizards.popartLanguageWizard.model.IPopartKeywordCodeGenerator;
+
+/**
+ * A code generator for PopartOperationKeywords.
+ * A PopartOperationKeywordCodeGenerator is capable of generating
+ * source code for a Popart language defintion from PopartOperationKeywords.
+ *
+ * @author David Marx
+ * @author Thorsten Peter
+ */
+public class PopartOperationKeywordCodeGenerator implements IPopartKeywordCodeGenerator {
+	
+	private PopartOperationKeyword keyword;
+	
+	/**
+	 * Constructs a new PopartOperationKeywordCodeGenerator for the
+	 * specified PopartOperationKeyword.
+	 * 
+	 * @param operationKeyword The PopartOperationKeyword
+	 */
+	public PopartOperationKeywordCodeGenerator(PopartOperationKeyword operationKeyword) {
+		this.keyword = operationKeyword;
+	}
+
+	/**
+	 * @see de.tud.stg.popart.eclipse.wizards.popartLanguageWizard.model.IPopartKeywordCodeGenerator
+	 */
+	public void create(IJavaProject project, IPackageFragment packagefragment,
+			IPackageFragmentRoot root, ArtifactCodeGenerator codeGenerator) {
+		
+		String returnType = keyword.getReturnType();
+		String name = keyword.getName();
+
+		if (returnType==null || returnType.equals("")) {
+			keyword.setReturnType("void");
+			returnType = keyword.getReturnType();
+		}
+		else {			
+			WizardUtil.createType(project, packagefragment, root, returnType, false);
+		}
+		
+		codeGenerator.addLineBreak();
+		codeGenerator.addCode(IndentationDirection.INDENT_RIGHT, "");
+		codeGenerator.addCode(annotation());
+		codeGenerator.addCode(
+				"public " + returnType + " " + name + "("
+						+ keyword.getParameterString() + ") {");
+		codeGenerator.addLineBreak();
+		codeGenerator.addLineBreak();
+		codeGenerator.addCode("}");
+		codeGenerator.addCode(IndentationDirection.INDENT_LEFT, "");
+
+	}
+
+	/**
+	 * Returns the annotation to set on this keyword.
+	 * 
+	 * @return The annotation string
+	 */
+	private String annotation() {
+		String result = "@PopartType(clazz=PopartOperationKeyword.class,breakpointPossible=";
+		result += (keyword.isBreakpointPossible()) ? 1 : 0;
+		result += ")";
+		return result;
+	}
+
+}
