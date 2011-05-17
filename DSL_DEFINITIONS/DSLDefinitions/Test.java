05e84503c53a61c079b98f77fcc0d4c8b3c8560b
Leo Roos <leor.email.googlemail.com>
2011-05-17 13:19:48 +0000

    all examples have been moved to the tigerseye-plugin trunk to folders named after their bundle identifier

--- DSL_DEFINITIONS/DSLDefinitions/Test.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println(Arrays.toString(de.tud.stg.popart.builder.test.dsls.SetDSL.class.getAnnotations()));
-	}
-}
