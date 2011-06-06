d2c38a456749dc96b517db427ccd4b05603ade66
Leo Roos <leor.email.googlemail.com>
2011-06-06 22:29:12 +0000

    added global test runner

---
+++ TigerseyeTestRunner/junit/de/tud/stg/tigesrseye/tests/AllTests.java
@@ -0,0 +1,19 @@
+package de.tud.stg.tigesrseye.tests;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.tud.stg.tigerseye.examples.test.AllTestsExampleDSLs;
+import de.tud.stg.tigerseye.test.AllTestsCoreTranformationTests;
+
+
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	AllTestsCoreTranformationTests.class,//
+	AllTestsExampleDSLs.class,//
+})
+
+public class AllTests {
+
+}
