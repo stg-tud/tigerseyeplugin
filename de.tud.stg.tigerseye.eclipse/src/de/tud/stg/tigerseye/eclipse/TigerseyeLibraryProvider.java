060f0e4309a8bd59e1f99f0733490ba0476a897f
Leo Roos <leor.email@googlemail.com>
2011-08-26 13:09:10 +0200

    removed unused dependencies

--- de.tud.stg.tigerseye.eclipse/src/de/tud/stg/tigerseye/eclipse/TigerseyeLibraryProvider.java
+++ de.tud.stg.tigerseye.eclipse/src/de/tud/stg/tigerseye/eclipse/TigerseyeLibraryProvider.java
@@ -2,15 +2,8 @@ package de.tud.stg.tigerseye.eclipse;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
 
 import org.eclipse.core.runtime.Plugin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import de.tud.stg.tigerseye.eclipse.dslsupport.TigerseyeSupportActivator;
 
